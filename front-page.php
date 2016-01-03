0d8c5b0cc2132877cfdf716d3f2cf35f4f894985
Robbe Vandendooren <robbe.vandendooren@gmail.com>
2016-01-03 09:35:35 +0100

    Initial commit

---
+++ front-page.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+<div class="container padding-0 margin-0">
+	<div class="row margin-0">
+		<div class="eyecatcher col-lg-12"></div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12 text-center cta">
+			<h1>The Company</h1>
+			<h2>One of us, One of them</h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-12 text-center padding-top">
+			<a class="button" href="posts.php">CHECK US OUT</a>
+		</div>
+	</div>
+</div>
+<!-- /.container -->
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+	<![endif]-->
+	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.js"></script>
+	<link href="<?php echo get_template_directory_uri(); ?>/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+	<script>
+		$(document).ready(function() {
+			console.log($("img"));
+			$("img").attr("class", "center-block").removeAttr("width").removeAttr("height").removeAttr("sizes").removeAttr("srcset");
+		});
+		//$("img").attr("class", "center-block");
+	</script>
+	<?php wp_head(); ?>
+</head>
+<body>
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse"
+			        data-target="#bs-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="nav-logo navbar-brand" href="index.php">
+				<img src="http://placehold.it/150x50&text=Logo" alt="">
+			</a>
+		</div>
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-right">
+				<li>
+					<a href="about.php">About</a>
+				</li>
+				<li>
+					<a href="contact.php">Contact</a>
+				</li>
+				<li>
+					<a href="http://iwtsl.ehb.be/~robbe.vandendooren/WDA/wordpress/posts/">Posts</a>
+				</li>
+			</ul>
+		</div>
+		<!-- /.navbar-collapse -->
+	</div>
+	<!-- /.container -->
+</nav>
